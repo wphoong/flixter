c385ab5d951775490dd8d72bcd79df1a0e3c8e84
Wilson <wphoong@gmail.com>
2017-04-29 02:02:05 +0000

    fixed awe photouploading

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :user
-  has_many :sections
   mount_uploader :image, ImageUploader
+  has_many :sections
 
   validates :title, presence: true
+  validates :image, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
