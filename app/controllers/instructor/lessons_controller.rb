ce8dbb89f9f69644ff8ff4220c1e6c7803c00e4a
Wilson <wphoong@gmail.com>
2017-04-30 01:32:13 +0000

    added custom middleware allowing only enrolled students to view lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -14,6 +14,7 @@ class Instructor::LessonsController < ApplicationController
 
   private
 
+
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render plain: 'Unauthorized', status: :unauthorized
