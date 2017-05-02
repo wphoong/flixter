d2700e76c3488b151c8d3e309b30062de970274f
Wilson <wphoong@gmail.com>
2017-05-02 03:48:44 +0000

    added simple use gem for ranking/ordering

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ gem 'mini_magick'
 gem 'carrierwave-aws'
 
 gem 'stripe'
+
+gem 'ranked-model'
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :section
 
   mount_uploader :video, VideoUploader
+
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end
