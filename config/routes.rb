fab298705f220de00931c856639cb041669e1b8a
Wilson <wphoong@gmail.com>
2017-05-03 05:11:01 +0000

    created route for dashboard and added content

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
      resources :courses, only: [:new, :create, :show] do
