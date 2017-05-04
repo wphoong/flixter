0d8a8d0488aeff360bc8a1646e2c9996625ca01d
Wilson <wphoong@gmail.com>
2017-05-04 02:56:43 +0000

    moved jquery ui in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require turbolinks
 //= require tether
 //= require bootstrap-sprockets
 //= require_tree .
-//= require jquery-ui
+
